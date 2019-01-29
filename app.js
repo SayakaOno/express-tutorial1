21771540571a92ec42e13c96c5cf84f15ee81829
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-01-29 10:49:38 -0800

    connect database

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 let express = require("express");
 let app = express();
+let db = require("./db");
 
-app.get("/", function(req, res) {
-  res.send("Hello World!");
-});
+module.exports = app;
 
-app.listen(3000, function() {
-  console.log("Example app listening on port 3000!");
-});
+// app.get("/", function(req, res) {
+//   res.send("Hello World!");
+// });
---
+++ db.js
@@ -0,0 +1,4 @@
+let mongoose = require("mongoose");
+import dbUri from "./data";
+
+mongoose.connect(dbUri);
