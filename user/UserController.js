09d0d69aa37dcde34eb7c97d08fd36ba71502fcf
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-01-29 15:31:06 -0800

    implement update and delete methods

--- user/UserController.js
+++ user/UserController.js
@@ -33,4 +33,37 @@ router.get("/", function(req, res) {
   });
 });
 
+router.get("/:id", function(req, res) {
+  User.findById(req.params.id, function(err, user) {
+    if (err) {
+      return res.status(500).send("There was a problem finding user.");
+    }
+    if (!user) {
+      return res.status(404).send("No user found.");
+    }
+    res.status(200).send(user);
+  });
+});
+
+router.delete("/:id", function(req, res) {
+  User.findByIdAndRemove(req.params.id, function(err, user) {
+    if (err) {
+      return res.status(500).send("There was a problem deleting user.");
+    }
+    res.status(200).send("User " + user.name + " was deleted");
+  });
+});
+
+router.put("/:id", function(req, res) {
+  User.findByIdAndUpdate(req.params.id, req.body, { new: true }, function(
+    err,
+    user
+  ) {
+    if (err) {
+      return res.status(500).send("There was a problem updating user.");
+    }
+    res.status(200).send(user);
+  });
+});
+
 module.exports = router;
